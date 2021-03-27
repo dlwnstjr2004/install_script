254c6fa79d70e20815dd113c83f55e5e496f3b9c
JuneA <dlwnstjr2004@naver.com>
2021-03-27 15:49:39 +0900

    xrdp update

--- x64/Xrdp/run.sh
+++ x64/Xrdp/run.sh
@@ -4,3 +4,9 @@ sudo apt-get install -y xorgxrdp
 sudo cp 45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/
 
 sudo service xrdp restart
+# 검은 화면일 때
+# sudo vim /etc/xrdp/startwm.sh
+
+#unset DBUS_SESSION_BUS_ADDRESS
+#unset XDG_RUNTIME_DIR
+#. $HOME/.profile
