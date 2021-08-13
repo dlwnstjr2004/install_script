fc7a82403d83ef3b418ae2c610a6a5d7e16b0237
JuneA <dlwnstjr2004@naver.com>
2021-08-13 18:01:19 +0900

    Xrdp update and only need run.sh

--- x64/Xrdp/run.sh
+++ x64/Xrdp/run.sh
@@ -1,12 +1,19 @@
 sudo apt-get install -y xorg-video-abi-23
 sudo apt-get install -y xrdp
 sudo apt-get install -y xorgxrdp
+
+echo "[Allow Colord all Users]" > 45-allow-colord.pkla
+echo "Identity=unix-user:*" >> 45-allow-colord.pkla
+echo "Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile" >> 45-allow-colord.pkla
+echo "ResultAny=no" >> 45-allow-colord.pkla
+echo "ResultInactive=no" >> 45-allow-colord.pkla
+echo "ResultActive=yes" >> 45-allow-colord.pkla
+
+sudo chmod 775 45-allow-colord.pkla
 sudo cp 45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/
 
-sudo service xrdp restart
-# 검은 화면일 때
-# sudo vim /etc/xrdp/startwm.sh
+sudo ufw allow 3389/tcp
 
-#unset DBUS_SESSION_BUS_ADDRESS
-#unset XDG_RUNTIME_DIR
-#. $HOME/.profile
+sudo perl -p -i -e '$.==28 and print "\nunset DBUS_SESSION_BUS_ADDRESS\nunset XDG_RUNTIME_DIR\n. $HOME/.profile\n"' /etc/xrdp/startwm.sh
+
+sudo service xrdp restart
